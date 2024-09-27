ad64c6a1adc544c2f4e9aab14e5b4adab5391c0e
gjm <gjm@techlogicals.uk>
2024-09-27 13:28:57 +0100

    new build and screen bugfix

---
+++ target/release/deps/autocfg-d7184e0f8f3cc4e0.d
@@ -0,0 +1,10 @@
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/deps/libautocfg-d7184e0f8f3cc4e0.rmeta: /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/lib.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/error.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/rustc.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/version.rs
+
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/deps/libautocfg-d7184e0f8f3cc4e0.rlib: /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/lib.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/error.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/rustc.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/version.rs
+
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/deps/autocfg-d7184e0f8f3cc4e0.d: /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/lib.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/error.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/rustc.rs /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/version.rs
+
+/home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/lib.rs:
+/home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/error.rs:
+/home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/rustc.rs:
+/home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.4.0/src/version.rs:
---
+++ target/release/linuxtoolbox.d
@@ -0,0 +1,1 @@
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/linuxtoolbox: /home/gjm/Documents/GitHub/LinuxToolbox/mainmenu.rs
