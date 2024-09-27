a3c797ceccdd2c5e4fde9dcc0673f15b742ea3e9
gjm <gjm@techlogicals.uk>
2024-09-27 13:21:38 +0100

    new bash

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -4,12 +4,12 @@
 detect_distro() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
-        echo $ID | tr '[:upper:]' '[:lower:]'
+        echo "$ID" | tr '[:upper:]' '[:lower:]'
     elif type lsb_release >/dev/null 2>&1; then
         lsb_release -si | tr '[:upper:]' '[:lower:]'
     elif [ -f /etc/lsb-release ]; then
         . /etc/lsb-release
-        echo $DISTRIB_ID | tr '[:upper:]' '[:lower:]'
+        echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]'
     elif [ -f /etc/debian_version ]; then
         echo "debian"
     else
@@ -17,29 +17,27 @@ detect_distro() {
     fi
 }
 
-# Function to install Lutris and dependencies
-install_lutris() {
+# Function to install Steam
+install_steam() {
     local distro=$1
     case $distro in
         ubuntu|debian)
-            sudo add-apt-repository ppa:lutris-team/lutris
+            sudo add-apt-repository multiverse
             sudo apt update
-            sudo apt install -y lutris
-            sudo apt install -y wine
+            sudo apt install -y steam
             ;;
         fedora)
-            sudo dnf install -y lutris
-            sudo dnf install -y wine
+            sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y steam
             ;;
         arch|manjaro)
-            sudo pacman -Sy lutris
-            sudo pacman -Sy wine
+            sudo pacman -Sy steam
             ;;
         opensuse|suse)
             sudo zypper addrepo https://download.opensuse.org/repositories/games/openSUSE_Leap_15.2/games.repo
             sudo zypper refresh
-            sudo zypper install -y lutris
-            sudo zypper install -y wine
+            sudo zypper install -y steam
             ;;
         *)
             echo "Unsupported distribution: $distro"
@@ -53,10 +51,12 @@ echo "Detecting Linux distribution..."
 DISTRO=$(detect_distro)
 echo "Detected distribution: $DISTRO"
 
-echo "Installing Lutris and dependencies..."
-install_lutris $DISTRO
+echo "Installing Steam..."
+install_steam $DISTRO
 
 echo "Verifying installation..."
-lutris --version
-
-echo "Lutris has been successfully installed."
+if command -v steam &> /dev/null; then
+    echo "Steam has been successfully installed."
+else
+    echo "Steam installation failed or not found in PATH."
+fi
