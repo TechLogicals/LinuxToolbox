d9744e5bc96c9297d6b8ee0af30c687d980982db
gjm <gjm@techlogicals.uk>
2024-10-03 16:24:28 +0100

    l;

---
+++ target/release/build/lock_api-387f8444abfb84bc/build_script_build-387f8444abfb84bc.d
@@ -0,0 +1,5 @@
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/build/lock_api-387f8444abfb84bc/build_script_build-387f8444abfb84bc: /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.12/build.rs
+
+/home/gjm/Documents/GitHub/LinuxToolbox/target/release/build/lock_api-387f8444abfb84bc/build_script_build-387f8444abfb84bc.d: /home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.12/build.rs
+
+/home/gjm/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.12/build.rs:
